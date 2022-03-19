c3a72f96c71b55bf4cd33d3b1bbae164664426e5
Petr Beranek <petrberanek.mail@gmail.com>
2022-03-19 12:07:05 +0100

    test(cnb-fxrates): run tests against offline data
    Querying only the CNB backend may not provide reproducible results.

--- tests/test_cnb_fxrates.py
+++ tests/test_cnb_fxrates.py
@@ -14,6 +14,7 @@ import pytest
 working_dir: str = os.path.dirname(os.path.realpath(__file__))
 prog_path: str = f"{working_dir}/../cnb-fxrates"
 decimal = re.compile(r"\d+\.\d{3}")
+sample_fxrates = f"{working_dir}/daily.txt"
 
 
 def test_no_args():
@@ -29,50 +30,56 @@ def test_no_args():
     assert "error" in proc.stderr.decode("utf-8").lower()
 
 
-@pytest.mark.online
-def test_currency_only():
-    """Test prog with only the CURRENCY arg.
+@pytest.mark.parametrize("query", ["mock", "online"])
+class TestQuery:
 
-    prog must return zero exit status and print the latest FX rate as
-    a decimal digit.
-    """
-    proc = subprocess.run([prog_path, "EUR"], capture_output=True)
-    assert proc.returncode == 0
-    assert decimal.match(proc.stdout.decode("utf-8"))
+    mock_url = {"CNB_FXRATES_URL": f"file://{sample_fxrates}"}
 
+    @pytest.mark.online
+    def test_currency_only(self, query):
+        """Test prog with only the CURRENCY arg.
 
-@pytest.mark.online
-def test_currency_and_date():
-    """Test prog with both the CURRENCY and the DATE args.
+        prog must return zero exit status and print the latest FX rate as
+        a decimal digit.
+        """
+        env = self.mock_url if query == "mock" else None
+        proc = subprocess.run([prog_path, "EUR"], capture_output=True, env=env)
+        assert proc.returncode == 0
+        assert decimal.match(proc.stdout.decode("utf-8"))
 
-    prog must return zero exit status and print the FX rate for the given
-    DATE as a decimal digit.
-    """
-    proc = subprocess.run([prog_path, "USD", "2022-02-01"], capture_output=True)
-    assert proc.returncode == 0
-    stdout = proc.stdout.decode("utf-8")
-    assert decimal.match(stdout)
-    assert stdout.strip() == "21.615"  # remove trailing newline
+    @pytest.mark.online
+    def test_currency_and_date(self, query):
+        """Test prog with both the CURRENCY and the DATE args.
 
+        prog must return zero exit status and print the FX rate for the given
+        DATE as a decimal digit.
+        """
+        env = self.mock_url if query == "mock" else None
+        proc = subprocess.run([prog_path, "USD", "2022-02-01"], capture_output=True, env=env)
+        assert proc.returncode == 0
+        stdout = proc.stdout.decode("utf-8")
+        assert decimal.match(stdout)
+        assert stdout.strip() == "21.615"  # remove trailing newline
 
-@pytest.mark.online
-@pytest.mark.parametrize("code", ["foo", "2022-01-01"])
-def test_invalid_currency_code(code):
-    """Test prog fails with an invalid CURRENCY code.
+    @pytest.mark.online
+    @pytest.mark.parametrize("code", ["foo", "2022-01-01"])
+    def test_invalid_currency_code(self, query, code):
+        """Test prog fails with an invalid CURRENCY code.
 
-    prog must return non-zero exit status and print an error msg into STDERR.
-    """
-    proc = subprocess.run([prog_path, code], capture_output=True)
-    assert proc.returncode == 1
-    assert "error" in proc.stderr.decode("utf-8").lower()
+        prog must return non-zero exit status and print an error msg into STDERR.
+        """
+        env = self.mock_url if query == "mock" else None
+        proc = subprocess.run([prog_path, code], capture_output=True, env=env)
+        assert proc.returncode == 1
+        assert "error" in proc.stderr.decode("utf-8").lower()
 
+    @pytest.mark.online
+    def test_flipped_currency_and_date(self, query):
+        """Test prog fails when CURRENCY and DATE args are flipped.
 
-@pytest.mark.online
-def test_flipped_currency_and_date():
-    """Test prog fails when CURRENCY and DATE args are flipped.
-
-    prog must return non-zero exit status and print an error msg into STDERR.
-    """
-    proc = subprocess.run([prog_path, "2022-02-01", "USD"], capture_output=True)
-    assert proc.returncode == 1
-    assert "error" in proc.stderr.decode("utf-8").lower()
+        prog must return non-zero exit status and print an error msg into STDERR.
+        """
+        env = self.mock_url if query == "mock" else None
+        proc = subprocess.run([prog_path, "2022-02-01", "USD"], capture_output=True, env=env)
+        assert proc.returncode == 1
+        assert "error" in proc.stderr.decode("utf-8").lower()
