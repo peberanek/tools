5d91611f700d6c568eb0cb8d017c61f50bdf4454
Petr Beranek <petrberanek.mail@gmail.com>
2022-03-23 18:42:18 +0100

    style(cnb-fxrates): apply black linter

--- cnb-fxrates.py
+++ cnb-fxrates.py
@@ -4,7 +4,12 @@ from datetime import date
 
 parser = argparse.ArgumentParser()
 parser.add_argument("currency", help="currency ISO code, e.g. USD")
-parser.add_argument("date", nargs='?', default=date.today().isoformat(), help="optional date in ISO format, e.g. 2022-01-01; default: today")
+parser.add_argument(
+    "date",
+    nargs="?",
+    default=date.today().isoformat(),
+    help="optional date in ISO format, e.g. 2022-01-01; default: today",
+)
 args = parser.parse_args()
 
 my_date = date.fromisoformat(args.date)
@@ -12,12 +17,12 @@ file_url = "https://www.cnb.cz/en/financial-markets/foreign-exchange-market/cent
 url_query = f"?date={my_date.strftime('%d.%m.%Y')}"
 
 with urllib.request.urlopen(file_url + url_query) as response:
-   data = response.read().decode("utf-8")  # FIXME: don't guess the data encoding
+    data = response.read().decode("utf-8")  # FIXME: don't guess the data encoding
 
 for line in data.split("\n")[2:]:  # skip ID and header lines
     if args.currency in line:
         fx_rate = line.split("|")[-1]
         break
 
-#print(data.split("\n")[:2])
+# print(data.split("\n")[:2])
 print(fx_rate)
