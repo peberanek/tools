90e62119bd6294ce5ba31dda38b29c2eab7869ce
Petr Beranek <petrberanek.mail@gmail.com>
2022-03-24 10:17:24 +0100

    feat(cnb-fxrates): validate currency code is valid
    Prevent bugs due to invalid currency code.

--- cnb-fxrates.py
+++ cnb-fxrates.py
@@ -1,6 +1,7 @@
 import argparse
 import urllib.request
 from datetime import date
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument("currency", help="currency ISO code, e.g. USD")
@@ -12,9 +13,48 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
+valid_currencies = [
+    "AUD",
+    "BGN",
+    "BRL",
+    "CAD",
+    "CHF",
+    "CNY",
+    "DKK",
+    "EUR",
+    "GBP",
+    "HKD",
+    "HRK",
+    "HUF",
+    "IDR",
+    "ILS",
+    "INR",
+    "ISK",
+    "JPY",
+    "KRW",
+    "MXN",
+    "MYR",
+    "NOK",
+    "NZD",
+    "PHP",
+    "PLN",
+    "RON",
+    "RUB",
+    "SEK",
+    "SGD",
+    "THB",
+    "TRY",
+    "USD",
+    "XDR",
+    "ZAR",
+]
 my_date = date.fromisoformat(args.date)
 file_url = "https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt"
 url_query = f"?date={my_date.strftime('%d.%m.%Y')}"
+
+if args.currency not in valid_currencies:
+    print(f"error: invalid currency code; valid codes: {' '.join(valid_currencies)}")
+    sys.exit(1)
 
 with urllib.request.urlopen(file_url + url_query) as response:
     data = response.read().decode("utf-8")  # FIXME: don't guess the data encoding
